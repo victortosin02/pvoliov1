ebb6ffe1a3039bf822244b8cb5b30fccb6bb695d
Victor <victortosin01@gmail.com>
2024-09-17 16:50:47 +0100

    Initial commit

--- app/animations/PreLoader/PreLoader.tsx
+++ app/animations/PreLoader/PreLoader.tsx
@@ -12,7 +12,7 @@ const PreLoader = () => {
     <div className="preloader gap-[5px] overflow-hidden text-[14px] sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]">
       <div className="texts-container w-500 flex h-60 items-center justify-center gap-[5px] overflow-hidden text-[14px] font-extrabold text-[#e4ded7] opacity-0 sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]">
         <span>Developer,</span>
-        <span>Designer,</span>
+        <span>Disruptor,</span>
         <span>Innovator.</span>
         <div className="sub hidden"></div>
       </div>
