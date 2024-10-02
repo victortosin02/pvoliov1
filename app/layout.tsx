23cefea539b99b3f653a126ac9af8de14c3c5112
Victor <victortosin01@gmail.com>
2024-10-02 17:31:28 +0100

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export const metadata: Metadata = {
     siteName: "Software Developer and Data Engineer",
     images: [
       {
-        url: "https://user-images.githubusercontent.com/84178696/228620835-e3cc5c9b-72fc-4f54-a628-407ef7b650f5.png",
+        url: "https://raw.githubusercontent.com/victortosin02/pvoliov1/main/uploads/motion%20tracker.png",
         width: 1200,
         height: 630,
         alt: "VictorOladejo - Software Developer and Data Engineer",
@@ -69,7 +69,7 @@ export const metadata: Metadata = {
       "Software Developer and Data Engineer. My strength lies in building AI powered applications.",
     creator: "@victortosin01",
     images: [
-      "https://user-images.githubusercontent.com/84178696/228620835-e3cc5c9b-72fc-4f54-a628-407ef7b650f5.png",
+      "https://raw.githubusercontent.com/victortosin02/pvoliov1/main/uploads/motion%20tracker.png",
     ],
   },
   robots: {
